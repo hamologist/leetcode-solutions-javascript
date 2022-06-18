a950196870cadf907e3a7d04a87889403d351fc0
Jordan Paris <hamologist@gmail.com>
2022-06-18 10:49:25 -0400

    Adds additional solutions
      - easy/contains-duplicate
      - medium/powx-n

---
+++ easy/contains-duplicate/main.test.ts
@@ -0,0 +1,15 @@
+import containsDuplicate from './main';
+
+const testCases: [number[], boolean][] = [
+    [[1, 2, 3, 1], true],
+    [[1, 2, 3, 4], false],
+    [[1, 1, 1, 3, 3, 4, 3, 2, 4, 2], true],
+];
+
+describe('contains-duplicates', () => {
+    testCases.forEach((testCase) => {
+        it(`when nums = ${testCase[0]}, should return ${testCase[1]}`, () => {
+            expect(containsDuplicate(testCase[0])).toStrictEqual(testCase[1]);
+        });
+    });
+});
--- easy/contains-duplicate/main.ts
+++ easy/contains-duplicate/main.ts
@@ -1,17 +1,16 @@
 function containsDuplicate(nums: number[]): boolean {
-  const lookup = new Map<number, boolean>();
+  const lookup = new Set<number>();
 
   for (const num of nums) {
     if (lookup.has(num)) {
       return true;
     }
 
-    lookup.set(num, true);
+    lookup.add(num);
   }
 
   return false;
 }
 
-console.log(containsDuplicate([1, 2, 3, 1]));
-console.log(containsDuplicate([1, 2, 3, 4]));
-console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+export default containsDuplicate;
+
